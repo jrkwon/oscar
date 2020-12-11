87d3f7b319ba5864ccf3cc1d36591fab1e2656d6
Jaerock Kwon <jaerock.kwon@gmail.com>
2020-12-10 23:21:45 -0500

    add err msg for not having OSCAR_PATH and showing config

--- neural_net/config.py
+++ neural_net/config.py
@@ -13,12 +13,19 @@ History:
 
 import os
 import yaml
+import sys
 
 class Config:
-    config_name = os.environ['OSCAR_PATH'] + '/config/neural_net/' + 'config.yaml'
+    try:
+        config_name = os.environ['OSCAR_PATH'] + '/config/neural_net/' + 'config.yaml'
+    except:
+        exit('ERROR: OSCAR_PATH not defined. Please source setup.bash.') 
+
     with open(config_name) as file:
         config_yaml = yaml.load(file, Loader=yaml.FullLoader)
         config_yaml_name = config_yaml['config_yaml']
+        print('=======================================================')
+        print('\tConfiguration Settings \n\t' + yaml.dump(config_yaml))
 
     yaml_name = os.environ['OSCAR_PATH'] + '/config/neural_net/' + config_yaml_name + '.yaml'
     with open(yaml_name) as file:
@@ -29,32 +36,11 @@ class Config:
     
     @staticmethod
     def summary():
-        print('============ neural_net configuration ============')
-        print('+ config name: ' + Config.config_yaml_name + '.yaml')
-        print('+ training ---------------------------------------')
-        print('-- network_type: ' + str(Config.config['network_type']))
-        print('-- lstm: ' + str(Config.config['lstm']))
-        print('-- validation_rate: ' + str(Config.config['validation_rate']))
-        print('-- num_epochs: ' + str(Config.config['num_epochs']))
-        print('-- batch_size: ' + str(Config.config['batch_size']))
-        print('-- num_outputs: ' + str(Config.config['num_outputs']))
-        print('+ steering angle preprocessing -------------------')
-        print('-- steering_angle_scale: ' + str(Config.config['steering_angle_scale']))
-        print('-- steering_angle_jitter_tolerance: '
-              + str(Config.config['steering_angle_jitter_tolerance']))
-        print('+ data augmentation ------------------------------')
-        print('-- data_aug_flip: ' + str(Config.config['data_aug_flip']))
-        print('-- data_aug_bright: ' + str(Config.config['data_aug_bright']))
-        print('-- data_aug_shift: ' + str(Config.config['data_aug_shift']))
-        print('-- image_size: ' + str(Config.config['input_image_width']) + 'x' \
-              + str(Config.config['input_image_height']))
-        print('+ data collection --------------------------------')
-        print('-- camera image topic: ' + Config.config['camera_image_topic'])
-        print('-- capture_area : ({0}, {1}) - ({2}, {3})'.format(
-                                  Config.config['image_crop_x1'],
-                                  Config.config['image_crop_y1'],
-                                  Config.config['image_crop_x2'],
-                                  Config.config['image_crop_y2']))
-        
+        print('=======================================================')
+        print('                 System Configuration ')
+        print('-------------------------------------------------------')
+        print(yaml.dump(Config.config))
+
+
 if __name__ == '__main__':
     Config.summary()
