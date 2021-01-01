50c9d474edbf3baa5fe30ede62cdf9e769e9771c
Jaerock Kwon <jaerock.kwon@gmail.com>
2020-12-31 21:45:22 -0500

    making video script using ffmpeg

---
+++ make_video.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Specify a data path where images are located..."  # modified DataSpeed track
+else 
+    rm $1.mp4
+    cat $1/*.jpg | ffmpeg -f image2pipe -framerate 17 -i - $1.mp4
+fi
