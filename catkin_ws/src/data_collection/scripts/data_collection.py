a7659b3b7df779b5e0ea5926d7f0f438d68a803d
kissw <kissw@github.com>
2021-05-17 21:53:36 +0900

    add save loss data

--- catkin_ws/src/data_collection/scripts/data_collection.py
+++ catkin_ws/src/data_collection/scripts/data_collection.py
@@ -59,11 +59,23 @@ class DataCollection():
         #path = '../data/' + sys.argv[1] + '/' + name_datatime + '/'
         # path = rospy.get_param('path_to_e2e_data', 
         #                 './e2e_data') + '/' + sys.argv[1] + '/' + name_datatime + '/'
-        path = sys.argv[1] + '/' + str(Config.neural_net['network_type']) + '/' + name_datatime + '/' 
-        if os.path.exists(path):
-            print('path exists. continuing...')
+        if sys.argv[1][-1] is '/':
+            argv = sys.argv[1]
         else:
+            argv = sys.argv[1] + '/'
+        n = 1
+        is_exist_path = argv + str(Config.neural_net['network_type']) + '/' + str(n) + 'n'
+        path = is_exist_path + '/' + name_datatime + '/'
+        
+        if os.path.exists(is_exist_path) is False:
             print('new folder created: ' + path)
+            os.makedirs(path)
+        else:
+            while(os.path.exists(is_exist_path)):
+                print('path exists. continuing...')
+                n += 1
+                is_exist_path = argv + str(Config.neural_net['network_type']) + '/' + str(n) + 'n'
+                path = is_exist_path + '/' + name_datatime + '/' 
             os.makedirs(path)
 
         self.text = open(str(path) + name_datatime + const.DATA_EXT, "w+")
