55e91beab8fbcdebb83a2df2dfa0050c8388deb0
Jesudara Omidokun <jesudaraomidokun@gmail.com>
2022-10-10 12:02:32 -0400

    updates

--- start_rover.sh
+++ start_rover.sh
@@ -11,4 +11,4 @@ cd ..
 #roslaunch rover mavros_posix_sitl.launch rviz:='true' x:=-9.88715 y:=8.23526 z:=0.411919 R:=-0.001258 P:=-0.010927 Y:=2.4979
 #roslaunch rover mavros_posix_sitl.launch rviz:='true' x:=7.58439 y:=-102.82 z:=0.02 R:=0 P:=0 Y:=0
 #roslaunch rover bridge_mavros2.launch rviz:='true' x:=0 y:=0 z:=0.179517 R:=-0.001258 P:=-0.010927 Y:=2.4979
-roslaunch rover bridge_mavros2o.launch
+roslaunch rover bridge_mavros2a.launch
